c903f400a9134692764ee301a2a9e4b8902e19c9
Md Shahnewaj Sheikh <shahanewajshaikh786@gmail.com>
2023-10-24 00:58:32 +0600

    Create class for Producer and Consumer

---
+++ ProducerConsumer/ProducerConsumer.java
@@ -0,0 +1,39 @@
+package com.sumit.playjava;
+
+//Utility class to perform wait and notify
+//Consumer class to consume
+class Consumer implements Runnable{
+    public Consumer(){
+
+    }
+
+    public void run(){
+        while (true){
+
+        }
+
+    }
+}
+
+
+
+
+//producer class to produce
+class Producer implements Runnable{
+    public Producer(){
+
+    }
+
+    public void run(){
+        while (true){
+
+        }
+
+    }
+}
+//create a min file to run producer and consumer
+public class ProducerConsumer {
+    public static void main(String[] args){
+
+    }
+}
